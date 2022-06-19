d964833897948562170fba9066a31af7ca78f25e
admin <admin@DESKTOP-FMNHA2I>
2022-06-19 20:01:42 +0530

    Commit Pattern

---
+++ src/Package1/Class1.java
@@ -0,0 +1,20 @@
+package Package1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		for(int a=1; a<=5; a++) {
+			for(int b=a; b<=5; b++) {
+				System.out.print(" ");
+			}
+			{
+				for(int c=1; c<=a;c++) {
+					System.out.print("*");
+				}
+					
+			}
+			
+			System.out.println();
+		}
+}
+}
